347979ca88fa1dac0f57084d30ec5f2343b6b31e
Bhagyesh Patil <118748705+bhagyesh98@users.noreply.github.com>
2024-10-04 15:14:34 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0ebfd941bbafe70c6"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "TerraformExample"
-  }
+resource "aws_instance" "one" {
+count = 2
+ami = "ami-0ebfd941bbafe70c6"
+instance_type = "t2.micro"
+tags = {
+Name = "${terraform.workspace}-server"
+}
 }
